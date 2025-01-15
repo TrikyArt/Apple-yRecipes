ba641b5bf22571614c93e29f6194ba1518653298
IlvieRath <cc231048@fhstp.ac.at>
2025-01-15 15:24:15 +0100

    Upadte MainActivity

--- app/src/main/java/com/example/apple_yrecipes/MainActivity.kt
+++ app/src/main/java/com/example/apple_yrecipes/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.apple_yrecipes
 
 import android.content.Intent
-import android.net.http.HeaderBlock
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,7 +13,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -23,12 +21,9 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
@@ -135,9 +130,14 @@ class MainActivity : ComponentActivity() {
                         },
                             colors = ButtonDefaults.buttonColors(
                                 colorResource(id = R.color.red)
-                            )
+                            ),
+                                modifier = Modifier.shadow(
+                                    elevation = 8.dp,
+                                    shape = RoundedCornerShape(16.dp),
+                                    clip = false
+                                )
 
-                        ) {
+                            ) {
                             Text(text = "Add Recipe")
                         } }
 

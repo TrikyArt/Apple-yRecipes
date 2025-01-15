586286b9c6dc0df8a2c2938ee917c87f96a87bca
Triky <lidia.a.klosowicz@gmail.com>
2025-01-15 13:35:47 +0100

    adding Image (no database)
    it be clickable tho

--- app/src/main/java/com/example/apple_yrecipes/RecipeActivity.kt
+++ app/src/main/java/com/example/apple_yrecipes/RecipeActivity.kt
@@ -1,13 +1,20 @@
 package com.example.apple_yrecipes
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
@@ -16,20 +23,29 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.room.Room
+import coil3.compose.rememberAsyncImagePainter
 import com.example.apple_yrecipes.ViewModel.CheckRecipeViewModel
 import com.example.apple_yrecipes.ViewModel.Repository
 import com.example.apple_yrecipes.db.Database
@@ -73,7 +89,6 @@ class RecipeActivity : ComponentActivity() {
 
                     })
 
-                    //TODO:: Add recipe information to thse variables
                     var RecipeName by remember {
                         mutableStateOf("")
                     }
@@ -131,9 +146,45 @@ class RecipeActivity : ComponentActivity() {
                                 Text(text = "Delete")
                             }
                         }
+                        RecipeImage()
                     }
                 }
             }
         }
+    }
+}
+
+@Composable
+fun RecipeImage(){
+
+    val imageUri = rememberSaveable {
+        mutableStateOf("")
+    }
+    val painter = rememberAsyncImagePainter(
+        imageUri.value.ifEmpty { R.drawable.ic_launcher_foreground }
+    )
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        uri?.let { imageUri.value = it.toString() }
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+        ) {
+        Image(painter = painter,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .clip(RectangleShape)
+                .size(250.dp)
+                .border(
+                    width = 1.dp,
+                    color = Color.Blue,
+                    shape = RectangleShape
+                    )
+                .clickable { launcher.launch("image/*") }
+        )
     }
 }

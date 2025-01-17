049a1550951b9e8c49f246ebb1941953b7afe36d
Triky <lidia.a.klosowicz@gmail.com>
2025-01-17 15:15:47 +0100

    minor changes

--- app/src/main/java/com/example/apple_yrecipes/EditActivity.kt
+++ app/src/main/java/com/example/apple_yrecipes/EditActivity.kt
@@ -1,58 +1,32 @@
 package com.example.apple_yrecipes
 
 import android.content.Intent
-import android.content.pm.ChangedPackages
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.room.Room
-import coil3.Image
-import coil3.compose.rememberAsyncImagePainter
 import com.example.apple_yrecipes.ViewModel.EditViewModel
 import com.example.apple_yrecipes.ViewModel.Repository
 import com.example.apple_yrecipes.db.AppDatabase
 import com.example.apple_yrecipes.ui.theme.AppleyRecipesTheme
-import java.io.File
-import java.io.FileOutputStream
-import java.io.InputStream
-import java.io.OutputStream
 
 class EditActivity : ComponentActivity() {
     private val db by lazy {
@@ -135,57 +109,3 @@ class EditActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun EditRecipeImage(imagePath: String, onImageChanged: (String) -> Unit)
-{
-    val context = LocalContext.current
-    val appImagesDir = File(context.filesDir, "appImages")
-    if (!appImagesDir.exists()) {
-        appImagesDir.mkdir()
-    }
-
-    val launcher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.GetContent()
-    ) { uri: Uri? ->
-        val inputStream: InputStream? = uri?.let { context.contentResolver.openInputStream(it) }
-        if (inputStream === null) {
-            Log.e("image", "no input stream")
-        }
-        val imageFileName = "image_${System.currentTimeMillis()}.jpg"
-        val imageFile = File(appImagesDir, imageFileName)
-
-        try {
-            val outputStream: OutputStream = FileOutputStream(imageFile)
-            inputStream?.copyTo(outputStream)
-            inputStream?.close()
-            outputStream.close()
-            onImageChanged(imageFile.absolutePath)
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
-    val painter = if (imagePath.isEmpty()) {
-        painterResource(R.drawable.ic_launcher_foreground)
-    } else {
-        rememberAsyncImagePainter(imagePath)
-    }
-
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Image(painter = painter,
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .clip(RectangleShape)
-                .size(250.dp)
-                .border(
-                    width = 1.dp,
-                    color = Color.Blue,
-                    shape = RectangleShape
-                )
-                .clickable { launcher.launch("image/*") }
-        )
-    }
-}
---
+++ app/src/main/java/com/example/apple_yrecipes/EditRecipeImage.kt
@@ -0,0 +1,78 @@
+package com.example.apple_yrecipes
+
+import android.net.Uri
+import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import coil3.compose.rememberAsyncImagePainter
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.io.OutputStream
+
+@Composable
+fun EditRecipeImage(imagePath: String, onImageChanged: (String) -> Unit)
+{
+    val context = LocalContext.current
+    val appImagesDir = File(context.filesDir, "appImages")
+    if (!appImagesDir.exists()) {
+        appImagesDir.mkdir()
+    }
+
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        val inputStream: InputStream? = uri?.let { context.contentResolver.openInputStream(it) }
+        if (inputStream === null) {
+            Log.e("image", "no input stream")
+        }
+        val imageFileName = "image_${System.currentTimeMillis()}.jpg"
+        val imageFile = File(appImagesDir, imageFileName)
+
+        try {
+            val outputStream: OutputStream = FileOutputStream(imageFile)
+            inputStream?.copyTo(outputStream)
+            inputStream?.close()
+            outputStream.close()
+            onImageChanged(imageFile.absolutePath)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+    val painter = if (imagePath.isEmpty()) {
+        painterResource(R.drawable.ic_launcher_foreground)
+    } else {
+        rememberAsyncImagePainter(imagePath)
+    }
+
+    Image(painter = painter,
+        contentDescription = null,
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .clip(RectangleShape)
+            .size(250.dp)
+            .border(
+                width = 1.dp,
+                color = colorResource(id = R.color.darkRed),
+                shape = RectangleShape
+            )
+            .clickable { launcher.launch("image/*") }
+    )
+}
